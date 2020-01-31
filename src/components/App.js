c527ed4dbfd7a901fb56adff26df2a9785dfeb6d
AveryJ13 <59802067+AveryJ13@users.noreply.github.com>
2020-01-31 16:09:36 -0700

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 import './App.css';
+import Post from './Post/Post'
+
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       posts: []
@@ -19,20 +21,28 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios.get('https://practiceapi.devmountain.com/api/posts').then(results => {
+      this.setState({posts: results.data})
+    })
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text}).then(results => {
+      this.setState({posts: results.data})
+    })
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`).then( results => {
+      this.setState({ posts: results.data });
+    });
   }
 
-  createPost() {
-
-  }
+  createPost(text) {
+    axios.post('https://practiceapi.devmountain.com/api/posts', { text }).then( results => {
+    this.setState({ posts: results.data });
+  } )
+}
 
   render() {
     const { posts } = this.state;
@@ -43,12 +53,22 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
-          
+          <Compose createPostFn = {this.createPost} />
+          {posts.map( post => 
+           (
+            <Post key={ post.id }
+            text = {post.text} 
+            date= {post.date}
+            id = {post.id}
+            updatePostFn = {this.updatePost}
+            deletePostFn = {this.deletePost}/>
+    ))
+            }
         </section>
       </div>
     );
   }
+
 }
 
 export default App;
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -54,7 +54,7 @@ export default class Post extends Component {
       // const editing = this.state.editing
       // const showMasterMenu = this.state.showMasterMenu
     const { editing, showMasterMenu } = this.state;
-
+    const { text, id, date, updatePostFn, deletePostFn} = this.props
     return (
       // Main body of post
       <section className="Post__parent" onClick={ this.hideMasterMenu }>
@@ -66,7 +66,7 @@ export default class Post extends Component {
           {/* Drop-down menu. Remember that the "showMasterMenu" variable has been destructured off of this.state */}
           <div className="Post__master-menu" style={ { display: showMasterMenu ? 'flex' : 'none' } }>
             <span onClick={ this.showEdit }>Edit</span>
-            <span>Delete</span>
+            <span onClick = {() => deletePostFn(id)}>Delete</span>
           </div>
         </div>
 
@@ -79,7 +79,7 @@ export default class Post extends Component {
           <span className="Post__name">DevMountain</span>
           <span className="Post__handle">@DevMountain</span>
 
-          <span className="Post__date">- POST DATE GOES HERE</span>
+          <span className="Post__date">- { date }</span>
         </div>
 
         {/* This is where the text goes. Notice the turnary statement. The turnary statement decides to display either the text OR the editor view
@@ -90,15 +90,17 @@ export default class Post extends Component {
                 <span ... ></span>
               }
         */}
-        <div className="Post__content">
+        <div className="Post__content" >
           {
             // This has been pulled off of this.state via destructuring
             editing
             ?
               <Edit text=""
-                    hideEdit={ this.hideEdit } />
+                    hideEdit= {this.hideEdit }
+                    updatePostFn = {updatePostFn} 
+                    id = {id}/>
             :
-              <span className="Post__text">POST TEXT GOES HERE</span>
+          <span className="Post__text">{text}</span>
           }
         </div>
 
